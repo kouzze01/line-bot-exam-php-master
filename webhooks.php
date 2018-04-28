ad2900879d7e030c87d70f201bfaabc5863236ae
Sontaya Sritorn <sontaya.sri@gmail.com>
2018-04-28 16:32:42 +0700

    -a

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			//$text = $event['source']['userId'];
-			$text = $event;
+			$text = "ควย";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
