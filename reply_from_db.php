65912599ca45abd0dc68db044c8f41dfd7ef282b
Sontaya Sritorn <sontaya.sri@gmail.com>
2018-04-29 01:07:17 +0700

    -a

--- reply_from_db.php
+++ reply_from_db.php
@@ -44,7 +44,7 @@ function replyFromDB($inputMsg) {
     //$returnResult = mysqli_num_rows($queryrResult);
     $row = $queryrResult->fetch_row();
     $rand_keys = array_rand($row, 1);
-    $textReply = $row[$rand_keys];
+    $textReply = $row[$rand_keys[0]];
     return $textReply['ReplyMassage'];
 
 
