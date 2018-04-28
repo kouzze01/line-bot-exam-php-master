fe71097ba546c08883e6ef301973a214d030ee9c
Sontaya Sritorn <sontaya.sri@gmail.com>
2018-04-28 21:19:38 +0700

    -a

--- reply_from_db.php
+++ reply_from_db.php
@@ -24,20 +24,20 @@ function replyFromDB($inputMsg) {
   $conn = new mysqli($server, $username, $password, $db);
 
   $result = $conn->query("SELECT ID,InputMassage,ReplyMassage FROM Line_DB where InputMassage=".$inputMsg)
-
-  if (mysql_num_rows($result)==0) {
-
-    mysql_close($dbhandle);
-    return $replyTeachMessage;
-  }else{
-    // Else Data Exits //
-    $rand_keys = array_rand($result, 1);
-    $textReply = $result[$rand_keys[0]];
-    //close the connection
-    mysql_close($dbhandle);
-    return $textReply{'ReplyMassage'};
-
-  }
+  return $result;
+  // if (mysql_num_rows($result)==0) {
+  //
+  //   mysql_close($dbhandle);
+  //   return $replyTeachMessage;
+  // }else{
+  //   // Else Data Exits //
+  //   $rand_keys = array_rand($result, 1);
+  //   $textReply = $result[$rand_keys[0]];
+  //   //close the connection
+  //   mysql_close($dbhandle);
+  //   return $textReply{'ReplyMassage'};
+  //
+  // }
 
 
 
